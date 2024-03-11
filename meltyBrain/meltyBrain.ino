59f25fe658cdde05a0549410fd1f6dea19c7fa74
Victor :) <68206827+TheVictorHaha@users.noreply.github.com>
2024-03-11 19:15:07 -0400

    new test functions/vars

--- meltyBrain/meltyBrain.ino
+++ meltyBrain/meltyBrain.ino
@@ -1,5 +1,7 @@
 #include <math.h>
 
+bool test = true;
+
 class motor {
   public:
     int positionOffset;
@@ -11,16 +13,21 @@ class motor {
     }
 
     void setSpeed(double speed){
-
+      //TBI
     }
 
     double SPEED_COEFFECIENT = 1;
-    void update(double angleReading, double angleInput, double inputSpeed, double throttle){
+    int update(double angleReading, double angleInput, double inputSpeed, double throttle){
       double currAngle = angleReading + positionOffset;
       currAngle = fmod(currAngle, 360.0);
       double reading = currAngle - angleInput + (currAngle - angleInput < 0 ? 360 : 0);
       double modifier = reading < 180 ? 1 : -1;
-      setSpeed(throttle + modifier * inputSpeed * SPEED_COEFFECIENT);
+      int speed = throttle + modifier * inputSpeed * SPEED_COEFFECIENT;
+      if(test){
+        return speed;
+      } else {
+        setSpeed();
+      }
     }
 };
 
@@ -31,16 +38,86 @@ void setup() {
   fwdAngle = 0;
 }
 
+//needs values
 int TRANSLATION_X_PIN;
 int TRANSLATION_Y_PIN;
 int THROTTLE_PIN;
 int ANCHOR_CHANGE_PIN;
 
+int inputY = 0;
+int inputX = 0;
+int simAngle = 0;
+bool testPrint = false;
+int testThrottle = 0;
+
+char printBuffer[128];
+
 void loop() {
   // put your main code here, to run repeatedly:
   updateSensors();
   updateDeltaTime();
-  takeInput(analogRead(TRANSLATION_X_PIN), analogRead(TRANSLATION_Y_PIN), analogRead(THROTTLE_PIN), analogRead(ANCHOR_CHANGE_PIN));
+
+  if(test){
+    
+    if(newData){
+      switch(receivedChar){
+        case 'u':
+          inputY = 1;
+          testThrottle = 1;
+        break;
+
+        case 'd':
+          inputY = -1;
+          testThrottle = 1;
+        break;
+
+        case 'l':
+          inputX = -1;
+          testThrottle = 1;
+        break;
+
+        case 'r':
+          inputX = 1;
+          testThrottle = 1;
+        break;
+
+        case 'x':
+          inputX = 0;
+          inputY = 0;
+          testThrottle = 0;
+        break;
+
+        case '1':
+          simAngle += 45;
+        break;
+
+        case '2':
+          simAngle -= 45;
+        break;
+
+        case 'p':
+          sprintf(printBuffer, "X: %d, Y: %d, ANGLE: %d\n", inputX, inputY, simAngle);
+          Serial.print(printBuffer);
+        break;
+
+        case 'P':
+          testPrint = !testPrint;
+        break;
+      }
+      takeInput(inputX, inputY, testThrottle, 0);
+    }
+  } else {
+    takeInput(analogRead(TRANSLATION_X_PIN), analogRead(TRANSLATION_Y_PIN), analogRead(THROTTLE_PIN), analogRead(ANCHOR_CHANGE_PIN));
+  }
+}
+
+char receivedChar;
+bool newData = false;
+void recvOneChar() {
+    if (Serial.available() > 0) {
+        receivedChar = Serial.read();
+        newData = true;
+    }
 }
 
 double deltaTime = 0;
@@ -62,6 +139,7 @@ double angularSpeed;
 void updateSensors(){
   double gyroOffset = 0;// work this into the angle calculation
   gyroAngle += gyroOffset;
+  //TBI
 }
 
 motor leftMotor(1, -90);
@@ -84,8 +162,14 @@ void takeInput(double translationX, double translationY, double throttle, double
   //Change input coming from square of possible x and y into circle of possible velocities
   double translationSpeed = translationY > translationX ? translationY : translationX;
 
-  leftMotor.update(gyroAngle, angle, translationSpeed, throttle);
-  rightMotor.update(gyroAngle, angle, translationSpeed, throttle);
+  int lSpeed = leftMotor.update(gyroAngle, angle, translationSpeed, throttle);
+  int rSpeed = rightMotor.update(gyroAngle, angle, translationSpeed, throttle);
+  if(test){
+    if(testPrint){
+      sprintf(printBuffer, "L: %d, R: %d\n", lSpeed, rSpeed);
+      Serial.print(printBuffer);
+    }
+  }
 
 }
 
